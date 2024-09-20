0cd6bf521a99426730fa5c33b69f3fbb92a25874
Efjulia <efjulia@mail.ru>
2024-09-20 20:12:43 +0300

    с возрастом

--- 1.py
+++ 1.py
@@ -2,13 +2,28 @@ import telebot;
 
 bot = telebot.TeleBot(token='7976932364:AAFEqk0224G30XpGg8176rTSZd-Md5M7xD8')
 
+
+global flag 
+
+flag = True
+
 @bot.message_handler(content_types=['text']) #слушаем бота
+
 def get_text(message):
-    if message.text == "Привет": #проверям сообщение от пользователя
-        bot.send_message(message.from_user.id, "Здравствуй, мой дорогой друг!") #отвечаем пользователю
+    global flag
+    if message.text == "/start": #проверям сообщение от пользователя
+        bot.send_message(message.from_user.id, "Здравствуй, мой дорогой друг! Представься...") #отвечаем пользователю
     elif message.text == "/help":
         bot.send_message(message.from_user.id, "напиши: Привет")
     else:
-        bot.send_message(message.from_user.id, "я тебя не понимаю, напиши '/help'")
+        if flag:
+            name = message.text
+            flag = False
+            bot.send_message(message.from_user.id, "Привет," + name + '\n Сколько тебе лет') #отвечаем пользователю
+            
+        else:
+            age = message.text
+            bot.send_message(message.from_user.id, "Твой возраст!" + age) #отвечаем пользователю
+        
 bot.polling(none_stop=True, interval=0)# бот постоянно будет опрашивает сервер
 
